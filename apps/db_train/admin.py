1b0bbffc640fb945356e5d7aedd59236e03c28a8
Egor <egorobidin@inbox.ru>
2024-10-10 00:41:39 +0300

    22

--- apps/db_train/admin.py
+++ apps/db_train/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from .models import Author
 # Зарегистрируйте свои модели в админ панели здесь
+admin.site.register(Author)
--- project/settings.py
+++ project/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'tinymce',
     'crispy_forms',
     "crispy_bootstrap4",
+    'apps.db_train_alternative',
 ]
 
 
